84d54eb3f59a4e173736b8602874a456041829c6
JCamiloo <jcamilo.osorio15@gmail.com>
2020-04-01 03:03:42 -0500

    database renamed

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ let connection;
 if (process.env.NODE_ENV === 'dev') {
   connection = 'mongodb://localhost:27017/coffeeShop'
 } else {
-  connection = 'mongodb+srv://juan:juan1234@cluster0-6u7vf.mongodb.net/test'
+  connection = 'mongodb+srv://juan:juan1234@cluster0-6u7vf.mongodb.net/coffeeShop'
 }
 
 process.env.CONNECTION = connection;
