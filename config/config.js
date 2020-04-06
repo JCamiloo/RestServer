d717d944a8a15a2e410c7d1ff3d5156d1149f00f
JCamiloo <jcamilo.osorio15@gmail.com>
2020-04-05 22:39:04 -0500

    google sign in added

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 process.env.TOKEN_CADUCITY = 60 * 60 * 24 * 30;
 process.env.TOKEN_SEED = process.env.TOKEN_SEED || 'development-seed';
+process.env.GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || '1026312298849-o43erq05v71sikoquc3gnb4bp7bd5nhg.apps.googleusercontent.com'; 
 
 let connection;
 
--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { OAuth2Client } = require('google-auth-library');
 const User = require('../models/user.model');
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 const app = express();
 
 app.post('/login', (req, res) => {
@@ -16,5 +19,52 @@ app.post('/login', (req, res) => {
     res.json({ success: true, data: dbUser, token });
   });
 });
+
+
+app.post('/google', async (req, res) => {
+  const token = req.body.idtoken;
+  const googleuser = await verify(token).catch(error => {
+    return res.status(403).json({ success: false, message: error });
+  });
+
+  User.findOne({ email: googleuser.email }, (error, dbUser) => {
+    if (error) return res.status(500).json({ success: false, message: error.message });
+    if (dbUser) {
+      if (!dbUser.google) {
+        return res.status(400).json({ success: false, message: 'User must use normal authentication' });
+      } else {
+        const token = jwt.sign({ user: dbUser }, process.env.TOKEN_SEED, { expiresIn: process.env.TOKEN_CADUCITY });
+        return res.json({ success: true, user: dbUser, token });
+      }
+    } else {
+      const user = new User();
+      user.name = googleuser.name;
+      user.email = googleuser.email;
+      user.image = googleuser.image;
+      user.google = true;
+      user.password = ':)';
+
+      user.save((error, userCreated) => {
+        if (error) return res.status(500).json({ success: false, message: error.message });
+        const token = jwt.sign({ user: dbUser }, process.env.TOKEN_SEED, { expiresIn: process.env.TOKEN_CADUCITY });
+        return res.json({ success: true, user: userCreated, token });
+      });
+    }
+  });
+});
+
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.GOOGLE_CLIENT_ID
+  });
+  const payload = ticket.getPayload();
+  return {
+    name: payload.name,
+    email: payload.email,
+    image: payload.picture,
+    google: true
+  }
+}
 
 module.exports = app;
