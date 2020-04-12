86395b5ab003de5f7b02568610000ea1571b5fae
JCamiloo <jcamilo.osorio15@gmail.com>
2020-04-11 23:43:00 -0500

    token caducity adjustment

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 process.env.PORT = process.env.PORT || 3000; 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
-process.env.TOKEN_CADUCITY = '7d';
+process.env.TOKEN_CADUCITY = '24h';
 process.env.TOKEN_SEED = process.env.TOKEN_SEED || 'development-seed';
 process.env.GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || '1026312298849-h4gr8qt00eco43rb3oh84l0tr9qmb43r.apps.googleusercontent.com'; 
 
