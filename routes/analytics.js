366096341be044b7bfc13a3ce40f9d88d6dc7ce3
JCamiloo <jcamilo.osorio15@gmail.com>
2020-05-23 12:29:49 -0500

    analytic endpoints

--- routes/analytics.js
+++ routes/analytics.js
@@ -3,9 +3,20 @@ const app = express();
 const axios = require('axios');
 const URL = 'http://34.71.9.156';
 
-
 app.get('/commerces/layer', async (req, res) => {
   const instance = axios.create({ baseURL: `${URL}/commerces/layer` });
+  const response = await instance.get();
+  res.json(response.data);
+});
+
+app.get('/commerces/graph', async (req, res) => {
+  const instance = axios.create({ baseURL: `${URL}/commerces/graph` });
+  const response = await instance.get();
+  res.json(response.data);
+});
+
+app.get('/commerces', async (req, res) => {
+  const instance = axios.create({ baseURL: `${URL}/commerces` });
   const response = await instance.get();
   res.json(response.data);
 });
